65e9f49154e73b4a9770bfda00dbb3975875e61a
Ramon Felipe Alves de Arruda Silva <ramon.felipe@hotmail.com>
2022-04-03 22:45:34 -0300

    Creation of Account class

---
+++ Projects/State/Account.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace State
+{
+    public class Account
+    {
+        public decimal Balance { get; private set; }
+        private bool IsVerified { get; set; }
+        private bool IsClosed { get; set; }
+        
+        public Action OnUnfreeze { get; }
+        private Action ManageUnfreezing { get; set; }
+
+
+        public Account(Action onUnfreeze)
+        {
+            this.OnUnfreeze = onUnfreeze;
+
+            this.ManageUnfreezing = this.StayUnfrozen;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            if (this.IsClosed)
+                return;
+
+            ManageUnfreezing();
+
+            this.Balance += amount;
+        }
+
+
+        public void Withdraw(decimal amount)
+        {
+            if (!this.IsVerified || this.IsClosed)
+                return;
+
+            ManageUnfreezing();
+
+            this.Balance -= amount;
+        }
+
+        private void StayUnfrozen()
+        {
+            Console.WriteLine("Not freezed");
+        }
+
+        private void Unfreeze()
+        {
+            this.OnUnfreeze();
+            this.ManageUnfreezing = this.StayUnfrozen;
+
+            Console.WriteLine("Unfreezed!");
+        }
+
+        public void HolderVerified()
+        {
+            this.IsVerified = true;
+        }
+
+        public void Close()
+        {
+            this.IsClosed = true;
+        }
+
+        public void Freeze()
+        {
+            if (this.IsClosed)
+                return;
+            if (!this.IsVerified)
+                return;
+
+            this.ManageUnfreezing = this.Unfreeze;
+            Console.WriteLine("Freezed!");
+        }
+    }
+}
---
+++ Projects/State/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace State
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+
+            var account = new Account(OnUnfreeze);
+
+            account.Deposit(10);
+            Console.WriteLine("Balance should be 10: {0}", account.Balance);
+            // balance = 10
+
+            account.HolderVerified();
+            account.Withdraw(1);
+            // balance = 9
+            Console.WriteLine("Balance should be 9: {0}", account.Balance);
+
+            account.Freeze();
+
+            account.Withdraw(2);
+            Console.WriteLine("Balance should be 7: {0}", account.Balance);
+            // balance = 7
+
+            account.Deposit(3);
+            Console.WriteLine("Balance should be 10: {0}", account.Balance);
+            // balance = 10
+
+            account.Close();
+            account.Deposit(1);
+            // balance = 10
+            account.Withdraw(1);
+            // balance = 10
+
+            Console.WriteLine("Balance after close account: {0}", account.Balance);
+
+
+            Console.ReadKey();
+        }
+
+        static void OnUnfreeze()
+        {
+            Console.WriteLine("OnUnfreeze called...");
+        }
+    }
+}
